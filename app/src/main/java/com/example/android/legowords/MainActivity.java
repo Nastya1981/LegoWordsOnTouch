68f09c2aff7b16742c679fd34d8282403008a62e
Nastya1981 <koshka_nastya@mail.ru>
2016-05-01 22:27:25 +0300

    create class WordsView for word

--- app/src/main/java/com/example/android/legowords/MainActivity.java
+++ app/src/main/java/com/example/android/legowords/MainActivity.java
@@ -2,43 +2,17 @@ package com.example.android.legowords;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity  implements WordsView.StatusListener{
-    private int mLevel = 0;
-    private String[] mArrayWords;
-    private String[] mArrayImages;
 
-
+    private WordsView mWordsView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        mArrayWords = getResources().getStringArray(R.array.words);
-        mArrayImages = getResources().getStringArray(R.array.images);
-        legoWord();
+        mWordsView = (WordsView) findViewById(R.id.word_view);
+        mWordsView.setStatusListener(this);
+        mWordsView.legoWord();
     }
-    public void legoWord(){
-        init();
-        setImageView(mArrayImages[mLevel]);
-        List <Character> shuffleLetters = shuffleLetters();
-        createLetters(shuffleLetters);
-        createContainerForLetters();
-    }
-
-
-
 }
--- app/src/main/java/com/example/android/legowords/WordsView.java
+++ app/src/main/java/com/example/android/legowords/WordsView.java
@@ -5,12 +5,10 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,146 +20,95 @@ public class WordsView extends RelativeLayout {
     private RelativeLayout mMainRelativeLayout;
     private ImageView mImageView;
     private TextView[] mContainerForLetters;
+
+    private int mLevel = 0;
+    private String[] mArrayWords;
+    private String[] mArrayImages;
+
     private int mcountLetter = 0;
     private LayoutInflater mInflater;
 
-
+    private int mPlaceSize;
     String printWord = "";
     String mWord;
     int mWordLength;
     char[] chars;
-    Button[] mLetters;
+    TextView[] mLetters;
 
     TextView txt2;
     private int _xDelta;
     private int _yDelta;
 
-
     public WordsView(Context context) {
         super(context);
+        init();
     }
 
     public WordsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public WordsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
-    mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    mInflater.inflate(R.layout.word_view, this, true);
-    mLinearLayoutLetters = (LinearLayout) findViewById(R.id.layout_letters);
-    mLinearLayoutForLetters = (LinearLayout) findViewById(R.id.layout_container_for_letters);
-    mMainRelativeLayout = (RelativeLayout) findViewById(R.id.mainLinearLayout);
-    mImageView = (ImageView)findViewById(R.id.image_word);
-    private StatusListener mStatusListener;
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        String str = "";
-        final int X = (int) event.getRawX();
-        final int Y = (int) event.getRawY();
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                _xDelta = X - lParams.leftMargin;
-                _yDelta = Y - lParams.topMargin;
-                break;
-            case MotionEvent.ACTION_UP:
-//                _view.setX(event.getX());
-//                _view.setY(event.getY());
-//
-                TextView containerLetter = mContainerForLetters[printWord.length()];
-                if((view.getY()<containerLetter.getY()&&view.getY()+25>=containerLetter.getY())||(view.getY()>containerLetter.getY()&&view.getY()-25<=containerLetter.getY())){
-                    if((view.getX()<containerLetter.getX()&&view.getX()+25>=containerLetter.getX())||(view.getX()>containerLetter.getX()&&view.getX()-25<=containerLetter.getX())){
-
-                        char letter = chars[printWord.length()];
-                        char buttonLetter = ((Button) view).getText().charAt(0);
-                        if (letter == buttonLetter) {
-                            containerLetter.setText(((Button) view).getText());
-                            mcountLetter++;
-                            printWord += ((Button) view).getText().toString();
-                            mMainRelativeLayout.removeView(view);
-                            if (printWord.length() == mWord.length()) {
-                                if(mLevel != 2) {
-                                    mLevel++;
-                                    mLinearLayoutForLetters.removeAllViews();
-                                    legoWord();
-                                }
-                            }
-                        }
-                        Toast.makeText(this, "Catch!", Toast.LENGTH_LONG).show();
-                    }
-                }
-                // if(mLetters[0].getY()<view.getY()-10){
-                // if(mLetters[0].getX()<view.getX()-100||mLetters[0].getX()>view.getX()+100){
-                //  Toast.makeText(this, "Catch!", Toast.LENGTH_LONG).show();
-                // }
-                //((Button) view).setText(String.valueOf(view.getY()) + "");
-                // mLetters[0].setText(String.valueOf(mLetters[0].getY())+" ");
-                //Button button = (Button) view;
-                //  ((Button) view).setText(String.valueOf(view.getY()) + "");
-                //  mLetters[0].setText(String.valueOf(mLetters[0].getY()) + "");
-                //((Button) view).setText(String.valueOf(view.getY()) + " " + String.valueOf(view.getX()) + " \n" + String.valueOf(view.getHeight()));
-
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                break;
-            case MotionEvent.ACTION_MOVE:
-                RelativeLayout.LayoutParams layoutParams  = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                layoutParams.leftMargin = X - _xDelta;
-                layoutParams.topMargin = Y - _yDelta;
-                layoutParams.rightMargin = -250;
-                layoutParams.bottomMargin = -250;
-                view.setLayoutParams(layoutParams);
-                break;
-        }
-        mLinearLayoutLetters.invalidate();
-//        _view2.setText(str);
-        return true;
-    }
     public void init(){
+        mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater.inflate(R.layout.word_view, this, true);
+
+        mLinearLayoutForLetters = (LinearLayout) findViewById(R.id.layout_container_for_letters);
+        mMainRelativeLayout = (RelativeLayout) findViewById(R.id.mainLinearLayout);
+        mImageView = (ImageView)findViewById(R.id.image_word);
+
+        mArrayWords = getResources().getStringArray(R.array.words);
+        mArrayImages = getResources().getStringArray(R.array.images);
+            /*???*/
+        mcountLetter = 0;
+    }
+
+    public void legoWord(){
+        if(mLevel > 0) {
+            mLinearLayoutForLetters.removeAllViewsInLayout();
+        }
+        printWord= "";
         mWord = mArrayWords[mLevel];
         mWordLength = mWord.length();
         chars = mWord.toCharArray();
-        mcountLetter = 0;
-        printWord= "";
+        setImageView(mArrayImages[mLevel]);
+        List <Character> shuffleLetters = shuffleLetters();
+        createLetters(shuffleLetters);
+        createContainerForLetters();
     }
-    public void setImageView(String image){
+
+    private void setImageView(String image){
         mImageView.setImageResource(this
                 .getResources()
-                .getIdentifier(image, "drawable", getContext()
+                .getIdentifier(mArrayImages[mLevel], "drawable", getContext()
                         .getPackageName()));
         mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
     }
 
     public void createLetters(List Letters){
-        mLetters = new Button[mWordLength];
-        Button letters;
-        float x = 150;
+        mLetters = new TextView[mWordLength];
+        TextView letter;
+        int x = startPositionLetter(mLetters.length);
         for (int i = 0; i < mLetters.length; i++) {
-            mLetters[i] = (Button)getLayoutInflater().inflate(R.layout.letter,null);
-            letters = mLetters[i];
-            letters.setText(String.valueOf(Letters.get(i)));
-            letters.setOnTouchListener(this);
-            mMainRelativeLayout.addView(letters);
-            letters.setX(x);
-            letters.setY(700);
-            letters.setId(i);
-            x += 80;
+            mLetters[i] = (TextView)mInflater.inflate(R.layout.letter,null);
+            letter = mLetters[i];
+            letter.setText(String.valueOf(Letters.get(i)));
+            letter.setOnTouchListener(mOnTouchListener);
+            mMainRelativeLayout.addView(letter);
+            letter.setX(x);
+            letter.setY(750);
+            x += 110;
         }
     }
-    public void createContainerForLetters(){
-        mContainerForLetters = new TextView[mWordLength];
-        TextView containerForLetters;
-        for (int i = 0; i < mContainerForLetters.length; i++) {
-            mContainerForLetters[i] = (TextView)getLayoutInflater().inflate(R.layout.container_for_letter,null);
-            containerForLetters = mContainerForLetters[i];
-            containerForLetters.setBackgroundResource(R.drawable.roundrect);
-            mLinearLayoutForLetters.addView(containerForLetters);
-        }
+    private int startPositionLetter(int lengthWord){
+        int x = 600 - (100 * lengthWord);
+        x = (x - (10 * lengthWord)) / 2;
+        return x;
     }
     public List shuffleLetters(){
         List<Character> Letters = new ArrayList<>();
@@ -173,6 +120,80 @@ public class WordsView extends RelativeLayout {
             Collections.shuffle(Letters);
         }
         return Letters;
+    }
+    public void createContainerForLetters(){
+        mContainerForLetters = new TextView[mWordLength];
+        TextView containerForLetters;
+        for (int i = 0; i < mContainerForLetters.length; i++) {
+            mContainerForLetters[i] = (TextView)mInflater.inflate(R.layout.letter,null);
+            containerForLetters = mContainerForLetters[i];
+            //containerForLetters.setBackgroundResource(R.drawable.roundrect);
+            // mLinearLayoutForLetters.addView(containerForLetters);
+            /*LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(mPlaceSize,mPlaceSize);
+            params.leftMargin = 10;
+            params.rightMargin = 10;*/
+            mLinearLayoutForLetters.addView(containerForLetters);
+        }
+    }
+    private void equalsLetters(char letterInWord, char TextViewLetter, TextView containerLetter, TextView letter){
+        if (letterInWord == TextViewLetter) {
+            containerLetter.setText(letter.getText());
+            mcountLetter++;
+            printWord += letter.getText().toString();
+            mMainRelativeLayout.removeView(letter);
+            if (printWord.length() == mWord.length()) {
+                if (mLevel != 2) {
+                    mLevel++;
+                    legoWord();
+                }
+            }
+        }
+    }
+    private StatusListener mStatusListener;
+    private OnTouchListener mOnTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            int access = 25;
+            TextView letter = (TextView) view;
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    TextView containerLetter = mContainerForLetters[printWord.length()];
+                    if ((view.getY() < mLinearLayoutForLetters.getY() && view.getY() + access >= mLinearLayoutForLetters.getY()) || (view.getY() > mLinearLayoutForLetters.getY() && view.getY() - access <= mLinearLayoutForLetters.getY())) {
+                       if ((view.getX() < containerLetter.getX() && view.getX() + access >= containerLetter.getX()) || (view.getX() > containerLetter.getX() && view.getX() - access <= containerLetter.getX())) {
+                            char letterInWord = chars[printWord.length()];
+                            char TextViewLetter = letter.getText().charAt(0);
+                            equalsLetters(letterInWord, TextViewLetter, containerLetter, letter);
+                            //   Toast.makeText(this, "Catch!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+                    layoutParams.rightMargin = 0;
+                    layoutParams.bottomMargin = 0;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            mMainRelativeLayout.invalidate();
+            return true;
+        }
+    };
+
+    public void setStatusListener(StatusListener statusListener) {
+        mStatusListener = statusListener;
     }
     public interface StatusListener {
     }
